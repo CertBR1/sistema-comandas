d39f1eb88429779ad816d671e522e82a6d6fe4de
CertBR1 <mateus.ribeiro3591@gmail.com>
2024-04-23 12:58:45 -0300

    implementado helmet na API

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
 
 async function bootstrap() {
   console.log(process.env.RMQ_URL);
@@ -9,6 +10,7 @@ async function bootstrap() {
   console.log(process.env.RMQ_DATABASE_QUEUE);
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger))
   await app.listen(3000);
